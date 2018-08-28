1a1fb62b20bbe461e057e8fc1d47c960acd1ada0
Victor <jvdelicia123@outlook.com>
2018-08-28 16:36:38 -0300

    Context Ready/Models start

---
+++ Myndie/Myndie/DAO/EntityContext.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Myndie.DAO {
+
+	[DbConfigurationType(typeof(MySqlEFConfiguration))]
+	public class EntityContext : DbContext {
+
+		//https://dev.mysql.com/doc/connector-net/en/connector-net-entityframework60.html  ----> LEVE ISSO PARA VIDA!
+		//public DbSet<User> Users { get; set; }
+		//public DbSet<ProdCategory> Categorys { get; set; }
+		//public DbSet<Product> Products { get; set; }
+
+		public EntityContext() : base("dbMyndie") { }
+		public EntityContext(DbConnection existingConnection, bool contextOwnsConnection) : base(existingConnection, contextOwnsConnection) { }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+			base.OnModelCreating(modelBuilder);
+		}
+
+	}
+}
---
+++ Myndie/Myndie/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Myndie.Models {
+	public class User {
+		public int Id { get; set; }
+		public int Login { get; set; }
+		public int Name { get; set; }
+		public int Pass { get; set; }
+		public int Email { get; set; }
+		public int BornDate { get; set; }
+		public int Pic { get; set; }
+		public int CrtDate { get; set; }
+		//FKs:
+		public int IdCountry { get; set; }
+		public int IdLang { get; set; }
+		public int IdDev { get; set; }
+		public int IdMod { get; set; }
+	}
+}
