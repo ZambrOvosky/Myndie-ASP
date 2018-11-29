442762ef37a037dcc9816af9442a3b1370d567e0
ZoeiroVader <ZoeiroVader@Vortex.com>
2018-11-29 20:17:38 -0200

    chatmobile
    cration of files

---
+++ Myndie/Scripts/chatmobile.js
@@ -0,0 +1,32 @@
+﻿
+
+
+let chatBox = document.querySelector(".chat-box");
+let toggleButton = document.querySelector(".chat-header button")
+let chatContent = document.querySelector(".chat-content");
+toggleButton.addEventListener('click', () => {
+    if (chatContent.style.maxHeight) {
+        chatContent.style.maxHeight = null;
+        chatContent.classList.remove('active');
+        toggleButton.innerText = "Show"
+        $('#smgs').text("");
+    } else {
+        chatContent.style.maxHeight = 500 + "px";
+        chatContent.classList.add('active');
+        toggleButton.innerText = "Hide"
+    }
+})
+// Outside click
+window.addEventListener('click', function (e) {
+    if (!chatBox.contains(e.target)) {
+        chatContent.style.maxHeight = null;
+        chatContent.classList.remove('active');
+        toggleButton.innerText = "Show";
+
+    }
+});
+(() => {
+    chatContent.style.maxHeight = 500 + "px";
+    chatContent.classList.add('active');
+    toggleButton.innerText = "Hide"
+})();
